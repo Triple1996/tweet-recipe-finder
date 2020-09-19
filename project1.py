b16d1e29741248423dff8cfa28208329c37e683f
EC2 Default User <ec2-user@ip-172-31-25-73.ec2.internal>
2020-09-19 18:37:53 +0000

    Retry uploading code for project1.py.

--- project1.py
+++ project1.py
@@ -16,3 +16,16 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth)
 
+app = flask.Flask(__name__)
+
+@app.route('/') # Python decorator
+def index():
+
+    return flask.render_template(
+        "index.html"
+        )
+    
+app.run(
+        port=int(os.getenv('PORT', 8080)),
+        host=os.getenv('IP', '0.0.0.0')
+)
