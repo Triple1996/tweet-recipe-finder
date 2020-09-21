ebc72b4dcbf675f3edc02ef311191a4f9b88329b
EC2 Default User <ec2-user@ip-172-31-25-73.ec2.internal>
2020-09-21 17:25:26 +0000

    Restructure lines to be more logical, add comments.

--- project1.py
+++ project1.py
@@ -8,32 +8,41 @@ import sys
 import flask
 import os
 
+# import API keys
 consumer_key=os.environ['API_KEY']
 consumer_secret=os.environ['API_SECRET_KEY']
 access_token=os.environ['ACCESS_TOKEN']
 access_token_secret=os.environ['ACCESS_TOKEN_SECRET']
 
-foods = ['Lasagna', 'Philly Cheese Steak', 'Smoked Salmon', 'Rotisserie Chicken', 'Hamburger', 'Quesadilla', 'Filet Mignon']
-
+# establish auth token
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth)
+
+# list of foods to choose from
+foods = ['Lasagna', 'Philly Cheese Steak', 'Smoked Salmon', 'Rotisserie Chicken', 'Hamburger', 'Quesadilla', 'Filet Mignon']
 
 app = flask.Flask(__name__)
 
 @app.route('/') # Python decorator
 def index():
     
+    # choose random food and query twitter
     randFood = random.choice(foods)
     result = auth_api.search(q=randFood, lang = "en", count=5)
+    
+    # if result is empty, pick a new food and query again
     while len(result) < 1:
         randFood = random.choice(foods)
         result = auth_api.search(q=randFood, lang = "en", count=5)
+        
+    # extract tweet information
     tweet = random.choice(result)
     tweetText = tweet.text
     tweetAuthor = tweet.user.name
     tweetDate = tweet.created_at
     
+    # pass parameters to flask to render webpage
     return flask.render_template(
         "index.html",
         tweet=tweetText,
